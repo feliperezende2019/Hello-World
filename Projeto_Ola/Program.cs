6ed79a37a23f1f06d8e5e43c3eb2c97a731a771a
Felipe Rezende <feliperezende2019@hotmail.com>
2021-09-21 00:34:08 -0300

    Projeto iniciado mostrando ola mundo

---
+++ Projeto_Ola/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Projeto_Ola {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Olá Mundão!");
+
+            Console.ReadLine();
+        }
+    }
+}
